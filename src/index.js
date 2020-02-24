50cb614b5e839a82cff1c67e71209878f4b80ce8
aleksanderbr <alexanderbraun@freenet.de>
2020-02-24 23:10:47 +0100

    "transition duration changed"

--- src/index.js
+++ src/index.js
@@ -3,24 +3,9 @@ import ReactDOM from 'react-dom';
 import './styles/style.css';
 import * as serviceWorker from './serviceWorker';
 import App from './components/App'
-import {BrowserRouter,Switch, Route} from 'react-router-dom'
-import Alpha from '../src/components/affine/Alpha'
 
 
-const repo = `/${window.location.pathname.split('/')[1]}`;
 
-
-const Root = () => {
-    return (
-        <BrowserRouter basename={repo}>
-            <Switch>
-                <Route path="/" exact component={App} />
-                <Route path="/fuck" component={Alpha} />
-            </Switch>
-        </BrowserRouter>
-    )
-}
-
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
