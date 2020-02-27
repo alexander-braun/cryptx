f51a9230c502b2f49aeec0e6b69fadf636409d1f
alexander-braun <alexanderbraun@freenet.de>
2020-02-27 16:49:38 +0100

    Updates

---
+++ precache-manifest.3b5039229d812aee0e49e55bdfb01ee3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24118bca20dec594fdfac740903b6b2a",
+    "url": "/strngcrypt/index.html"
+  },
+  {
+    "revision": "b83e7ad72d4283d89aae",
+    "url": "/strngcrypt/static/css/2.36ab2c59.chunk.css"
+  },
+  {
+    "revision": "12bedf29322a806088fd",
+    "url": "/strngcrypt/static/css/main.207f4a56.chunk.css"
+  },
+  {
+    "revision": "b83e7ad72d4283d89aae",
+    "url": "/strngcrypt/static/js/2.1eefa0e5.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/strngcrypt/static/js/2.1eefa0e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12bedf29322a806088fd",
+    "url": "/strngcrypt/static/js/main.162b4198.chunk.js"
+  },
+  {
+    "revision": "9cb06f7a0491ad476fde",
+    "url": "/strngcrypt/static/js/runtime-main.b46013db.js"
+  },
+  {
+    "revision": "2961acbb4ea3d72f6699a6ccf6e03a66",
+    "url": "/strngcrypt/static/media/caret.2961acbb.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/strngcrypt/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/strngcrypt/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/strngcrypt/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/strngcrypt/static/media/slick.f97e3bbf.svg"
+  }
+]);
