8a69aa12edab379a3a941e117740135755c6fb36
alexander-braun <alexanderbraun@freenet.de>
2020-03-15 16:36:40 +0100

    Updates

--- precache-manifest.9843a3887e2e177f4e735b65d6a04e55.js
+++ precache-manifest.9843a3887e2e177f4e735b65d6a04e55.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f161b7979d5860bbc597611369381742",
+    "revision": "884a37e1f739b47de6b7a57b76c40cf5",
     "url": "/strngcrypt/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/strngcrypt/static/css/2.36ab2c59.chunk.css"
   },
   {
-    "revision": "fa077553ab4c1de21391",
-    "url": "/strngcrypt/static/css/main.a15c6eab.chunk.css"
+    "revision": "1caca6808df704eec3fd",
+    "url": "/strngcrypt/static/css/main.0d73cf3a.chunk.css"
   },
   {
     "revision": "842b4027172b7ec7a690",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/strngcrypt/static/js/2.5f6322ff.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fa077553ab4c1de21391",
-    "url": "/strngcrypt/static/js/main.00cca111.chunk.js"
+    "revision": "1caca6808df704eec3fd",
+    "url": "/strngcrypt/static/js/main.4ef23eab.chunk.js"
   },
   {
     "revision": "9cb06f7a0491ad476fde",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/strngcrypt/precache-manifest.34c68c827610b6e5f791a308a35a2fd2.js"
+  "/strngcrypt/precache-manifest.9843a3887e2e177f4e735b65d6a04e55.js"
 );
 
 self.addEventListener('message', (event) => {
