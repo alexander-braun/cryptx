085a15b6b921f8b659277ae8f7b13bc8afc7246a
alexander-braun <alexanderbraun@freenet.de>
2020-03-18 01:10:16 +0100

    added seperator

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,9 @@ function App() {
     <>
       <Header />
       <Hero />
+      <svg id="seperatorHeader" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+        <polygon fill="#4597c7" points="0,100 100,0 100,100" />
+      </svg>
       <BlockElementCollector />
     </>
   );
