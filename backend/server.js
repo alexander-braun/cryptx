5d01d40c95ecbee8344dd62bc41aee833c3b8a65
alexander-braun <alexanderbraun@freenet.de>
2020-04-14 11:37:37 +0200

    heroku tryout

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express()
 
@@ -9,7 +10,7 @@ connectDB()
 //Init Middleware
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => res.send('API RUNNING'))
+//app.get('/', (req, res) => res.send('API RUNNING'))
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'))
@@ -17,6 +18,13 @@ app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/presets', require('./routes/api/presets'))
+
+//Serve static assets in production
+
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    });
 
 const PORT = process.env.PORT || 5000
 
