1194c7ce206cdc49e085bfd72800fd4607710e22
alexander-braun <alexanderbraun@freenet.de>
2020-06-30 16:25:05 +0200

    otp transformed to bem scss

--- client/src/actions/profiles.js
+++ client/src/actions/profiles.js
@@ -10,7 +10,6 @@ export const getCurrentProfile = () => async (dispatch) => {
       payload: res.data,
     });
   } catch (error) {
-    console.log(error);
     dispatch({
       type: PROFILE_ERROR,
       payload: {
--- client/src/components/encryption_methods/onetimepad/otpGenerate.js
+++ client/src/components/encryption_methods/onetimepad/otpGenerate.js
@@ -6,55 +6,60 @@ import setOtpKey from '../../../actions/setOtpKey';
 class Otp extends React.PureComponent {
   constructor(props) {
     super(props);
-
     this.genRandomKey = this.genRandomKey.bind(this);
   }
 
+  /**
+   * Set a key for the initial calculation.
+   */
   componentDidMount() {
     this.props.setOtpKey(this.genRandomKey());
   }
 
+  /**
+   * Method to generate random characters fitting the input length.
+   * Nonalphabetic characters are not beeing encoded.
+   */
   genRandomKey = () => {
     let randomArr = [];
     let letters = this.props.alphabet.split('');
-
     let input = [];
     for (let i = 0; i < this.props.input.length; i++) {
       if (this.props.alphabet.indexOf(this.props.input[i] !== -1)) {
         input.push(this.props.input[i]);
       }
     }
-
     let inputLength = input.length;
-
     for (let i = 0; i < inputLength; i++) {
       randomArr.push(letters[Math.floor(Math.random() * 26)]);
     }
-
     return randomArr.join('');
   };
 
   render() {
     return (
-      <div className='controller'>
-        <div className='settings_name'>GENERATE RANDOM KEY</div>
-        <div className='settings_operators' id='genRandomTab'>
-          <div className='settings_operator' id='otpKey'>
-            {this.props.otpKey}
+      <div className='contentbox'>
+        <div className='content-element'>
+          <div className='content-element__settings-name'>
+            GENERATE RANDOM KEY
           </div>
-          <button
-            id='generate'
-            className='settings_operator'
-            onClick={() => {
-              this.props.setOtpKey(this.genRandomKey());
-            }}
-          >
-            Generate new random key
-          </button>
-          <div id='caesar_explanatory_text'>
-            <p className='feature_text'>
+          <div className='content-element__settings-operators content-element__settings-operators--vertical-center-flex'>
+            <div className='content-element__single-operator'>
+              {this.props.otpKey}
+            </div>
+            <button
+              id='generate'
+              className='content-element__single-operator content-element__single-operator--button'
+              onClick={() => {
+                this.props.setOtpKey(this.genRandomKey());
+              }}
+            >
+              Generate new random key
+            </button>
+            <p className='content-element__feature_text'>
               For the purpose of trying out de- and encryption with the same
-              key, the key will not update when your input message updates.
+              key, the key will not update when your input message updates. A
+              real one-time-pad key updates with every new message.
             </p>
           </div>
         </div>
